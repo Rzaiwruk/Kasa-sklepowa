2780d816cae906ba15375f3880717379b05895a8
Rzaiwruk <rzaiwruk@gmail.com>
2025-01-21 21:02:27 +0100

    Add files via upload

---
+++ Kasa sklepowa/Kasa sklepowa/Kasa sklepowa/Lista.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kasa_sklepowa
+{
+    public class Produkt
+    {
+        public int Index { get; set; }
+        public string Nazwa { get; set; }
+        public double Cena { get; set; }
+
+        public Produkt(int index, string nazwa, double cena)
+        { 
+            Index = index;
+            Nazwa = nazwa;
+            Cena = cena;
+        }
+    }
+    public class ListaProduktow
+    {
+        public List<Produkt> produkty = new List<Produkt>();
+
+        public void Dodaj(int index, string nazwa, double cena)
+        {
+            produkty.Add(new Produkt(index, nazwa, cena));
+        }
+
+        public void WyswietlListe()
+        {
+            foreach (var produkt in produkty) Console.WriteLine($"Numer: {produkt.Index} Nazwa: {produkt.Nazwa} Cena: {produkt.Cena}");
+            return;
+        }
+    public void UsunProdukt(int index)
+    {
+        var usun = produkty.Find(x => x.Index == index);
+
+            if (usun != null)
+            {
+                produkty.Remove(usun);
+                Console.WriteLine($"Produkt o numerze {index} został usunięty.");
+            }
+            else Console.WriteLine($"Produkt o numerze {index} nie istnieje na liście.");
+        }
+    }
+
+}
---
+++ Kasa sklepowa/Kasa sklepowa/Kasa sklepowa/Program.cs
@@ -0,0 +1,54 @@
+﻿using Kasa_sklepowa;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+ListaProduktow lista = new ListaProduktow();
+
+Menu.PokazMenu();
+while (true)
+{
+    var x = Console.ReadLine();
+    int y = int.Parse(x);
+    switch (y)
+    {
+        case 1:
+            Console.Clear();
+            Console.WriteLine("Produkty: ");
+            lista.WyswietlListe();
+            Menu.PokazMenu();
+            break;
+        case 2:
+            Console.Clear();
+            Console.WriteLine("Podaj numer produktu, który chcesz dodać: ");
+            var a = Console.ReadLine();
+            int index = int.Parse(a);
+            Console.WriteLine("Podaj nazwę produktu, który chcesz dodać: ");
+            var nazwa = Console.ReadLine();
+            Console.WriteLine("Podaj cenę produktu, który chcesz dodać: ");
+            var b = Console.ReadLine();
+            double cena = double.Parse(b);
+            lista.Dodaj(index, nazwa, cena);
+            Menu.PokazMenu();
+            break;
+        case 3:
+            Console.Clear();
+            Console.WriteLine("Podaj numer produktu, który chcesz usunać: ");
+            var c = Console.ReadLine();
+            int usun = int.Parse(c);
+            lista.UsunProdukt(usun);
+            Menu.PokazMenu();
+            break;
+        case 4:
+            Console.WriteLine("Dziękuję :)");
+            return;
+        default:
+            Console.WriteLine("Podałeś złą liczbę. Podaj jeszcze raz.");
+            Menu.PokazMenu();
+            break;
+    }
+}
+Console.ReadLine();
