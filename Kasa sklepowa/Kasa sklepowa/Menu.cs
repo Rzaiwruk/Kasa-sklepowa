d4c8869291e88e0bb8be942563c4ebf7063802b7
Rzaiwruk <rzaiwruk@gmail.com>
2025-01-21 21:05:40 +0100

    Add files via upload

---
+++ Kasa sklepowa/Kasa sklepowa/Menu.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kasa_sklepowa
+{
+    public class Menu
+    {
+        public static void PokazMenu()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Lista produktów");
+            Console.WriteLine("Co chcesz zrobić?");
+            Console.WriteLine("1. Wyświetl wszystkie produkty");
+            Console.WriteLine("2. Dodaj produkt");
+            Console.WriteLine("3. Usuń produkt");
+            Console.WriteLine("4. Wyjdź");
+            Console.WriteLine();
+        }
+    }
+}
